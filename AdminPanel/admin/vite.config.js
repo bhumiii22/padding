40bfbd5458b8a204914eb1617479a9ac27a88ebc
bhumiii22 <bhumikajinnah@gmail.com>
2026-03-17 22:38:14 +0530

    Update vite.config.js

--- AdminPanel/admin/vite.config.js
+++ AdminPanel/admin/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensures assets are loaded from the root
+  build: {
+    outDir: 'dist', // Explicitly tells Vite to put files in 'dist'
+    assetsDir: 'assets', // Keeps your JS/CSS organized in an assets folder
+  }
 })
